5e639ecb7a1317dd6199a6d2a6c9c5ddf577e6e5
Manoj Shrestha <shresthanewar678@gmail.com>
2024-09-03 07:57:47 +0545

    feat: the dustbin open notification

--- assets/project/smart_Dustbin/script/script.js
+++ assets/project/smart_Dustbin/script/script.js
@@ -1,37 +1,48 @@
-setInterval(()=>{
-    fetch(
-        "https://sgp1.blynk.cloud/external/api/get?token=-Suu9pGlY3Kw9uNTWdM726riQP9Ww_jn&v0"
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data)
-          const garbageLevel = data
-            ? (((23 - data) * 100) / 23).toFixed(2)
-            : "N/A";
-          if (garbageLevel > 100) {
-            document.getElementById(
-              "garbageLevel"
-            ).innerText = `Garbage Level: 100 %`;
-          } else if (garbageLevel < 0) {
-            document.getElementById(
-              "garbageLevel"
-            ).innerText = `Garbage Level: 0 %`;
-          } else {
-            if(garbageLevel > 80){
-              document.getElementById("garbageLevel").style.backgroundColor = "red";
-            }else{
-              document.getElementById("garbageLevel").style.backgroundColor = "white";
-            }
-            document.getElementById(
-              "garbageLevel"
-            ).innerText = `Garbage Level: ${garbageLevel} %`;
-          } // Getting garbage level or 'N/A' if not available
-        })
-        .catch((error) => {
-          console.error("Error fetching garbage level:", error);
-          document.getElementById("garbageLevel").innerText = "Error fetching data";
-        });
-},1000);
+setInterval(() => {
+  fetch(
+    "https://sgp1.blynk.cloud/external/api/get?token=-Suu9pGlY3Kw9uNTWdM726riQP9Ww_jn&v0"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      if (data === -1) {
+        document.getElementById("garbageLevel").innerText =
+          "The dustbin lid is open";
+        document.getElementById("garbageLevel").style.backgroundColor = "yellow";
+        return;
+      } else if (data > 23) {
+        document.getElementById("garbageLevel").innerText =
+          "The dustbin lid may be open";
+        document.getElementById("garbageLevel").style.backgroundColor = "orange";
+        return;
+      }
+
+      const garbageLevel = data
+        ? (((23 - data) * 100) / 23).toFixed(2)
+        : "N/A";
+
+      if (garbageLevel > 100) {
+        document.getElementById("garbageLevel").innerText =
+          `Garbage Level: 100 %`;
+      } else if (garbageLevel < 0) {
+        document.getElementById("garbageLevel").innerText =
+          `Garbage Level: 0 %`;
+      } else {
+        if (garbageLevel > 80) {
+          document.getElementById("garbageLevel").style.backgroundColor = "red";
+        } else {
+          document.getElementById("garbageLevel").style.backgroundColor = "white";
+        }
+        document.getElementById("garbageLevel").innerText =
+          `Garbage Level: ${garbageLevel} %`;
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching garbage level:", error);
+      document.getElementById("garbageLevel").innerText = "Error fetching data";
+    });
+}, 1000);
+
 const btn = document.querySelector("#btn");
 btn.addEventListener("click", () => {
   if (btn.innerText === "Open") {
